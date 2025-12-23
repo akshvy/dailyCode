0f24316ce00a59875744df4afd2c7d5cbb59170d
akshvy <akshayroy217@gmail.com>
2025-12-23 22:06:16 +0530

    Add files via upload

---
+++ A_Elephant.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int x;
+    cin >> x;
+     int total = 0;
+     while (x){
+        if (x == 1 || x ==2 || x ==3 || x == 4 || x == 5){
+            total += 1;
+            break;
+        }
+        else{
+            x -= 5;
+            total += 1;
+            
+        }
+        
+     }
+     cout << total << endl;
+    return 0;
+    
+}
