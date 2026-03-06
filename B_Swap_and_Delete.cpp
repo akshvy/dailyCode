3448f055c215ecdca21bf7f88d63c683e7e4b96e
akshvy <akshayroy217@gmail.com>
2026-03-06 09:30:20 +0530

    solves swap and delete(1000 rating question)

--- B_Swap_and_Delete.cpp
+++ B_Swap_and_Delete.cpp
@@ -1,30 +1,39 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
-
 using namespace std;
 
 void solve() {
     string s;
     cin >> s;
+    int n = s.size();
+    
     int c0 = 0, c1 = 0;
     for (char c : s) {
-        if (c == '0') c0++;
-        else c1++;
+        if (c == '0') c0++; else c1++;
     }
-
-    if (c0 == c1) {
-        cout << 0 << endl;
-    } else {
-        cout << abs(c0 - c1) << endl;
+    
+    // Find maximum k such that:
+    // O_k (# of 1s in s[0..k-1]) <= c0
+    // Z_k (# of 0s in s[0..k-1]) <= c1
+    // Binary search or linear scan
+    
+    int maxK = 0;
+    int ones = 0, zeros = 0;
+    for (int k = 1; k <= n; k++) {
+        if (s[k-1] == '1') ones++; else zeros++;
+        if (ones <= c0 && zeros <= c1) {
+            maxK = k;
+        }
     }
+    
+    cout << (n - maxK) << "\n";
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int t;
-    if (!(cin >> t)) return 0;
-    while (t--) {
-        solve();
-    }
+    cin >> t;
+    while (t--) solve();
     return 0;
 }
