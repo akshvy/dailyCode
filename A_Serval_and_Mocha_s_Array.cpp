914e5d64725d8e815f549da69d3743bc8a7c770d
akshvy <akshayroy217@gmail.com>
2026-06-11 10:48:45 +0530

    Refactor A. Serval and Mocha's Array implementation for improved readability and consistency; update output messages to uppercase.

--- A_Serval_and_Mocha_s_Array.cpp
+++ A_Serval_and_Mocha_s_Array.cpp
@@ -1,10 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main (){
-    int t;
+int main()
+{
+    int t; 
     cin >> t;
-    while(t--){
-          
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+        vector<long long> a(n); 
+        for (int i = 0; i < n; i++) 
+            cin >> a[i];
+
+        int flag = 0; 
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = i + 1; j < n; j++) 
+            {
+               
+                if (__gcd(a[i], a[j]) <= 2) 
+                {
+                    flag = 1; 
+                }
+            }
+        }
+
+        
+        if (flag == 0)
+            cout << "no" << endl;
+        else
+            cout << "yes" << endl;
     }
+    return 0;
 }
