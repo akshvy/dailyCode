11dcefcc9df1667ebe9b2f6ec8305ea6469de682
akshvy <akshayroy217@gmail.com>
2026-07-17 23:38:15 +0530

    Fix implementation for A. Everybody Likes Good Arrays problem by completing the logic for even and odd counts

--- A_Everybody_Likes_Good_Arrays.cpp
+++ A_Everybody_Likes_Good_Arrays.cpp
@@ -7,6 +7,21 @@ int main(){
     while(t--){
         int n;
         cin>>n;
-        vector 
+        long long a[n];
+        for(int i = 0; i < n; i++)
+            cin>>a[i];
+        
+            int count_even = 0;
+            int count_odd = 0;
+            for(int i = 0; i < n; i++){
+                if(a[i] % 2 == 0)
+                    count_even++;
+                else
+                    count_odd++;
+            }
+            if(count_even == count_odd)
+                cout<<"0"<<endl;
+            else if(count_even > count_odd)
+                
     }
 }
