2063a9ee86f479131491570f4abf5ae5cb9476c4
akshvy <akshayroy217@gmail.com>
2026-07-17 23:48:25 +0530

    Refactor A. Everybody Likes Good Arrays implementation for improved formatting and clarity

--- A_Everybody_Likes_Good_Arrays.cpp
+++ A_Everybody_Likes_Good_Arrays.cpp
@@ -1,27 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     int t; 
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        long long a[n];
-        for(int i = 0; i < n; i++)
-            cin>>a[i];
+    cin >> t;
+    while (t--)
+    {
+        long long n; 
+        cin >> n;
+        long long a[n]; 
+        for (long long i = 0; i < n; i++) 
+            cin >> a[i];
         
-            int count_even = 0;
-            int count_odd = 0;
-            for(int i = 0; i < n; i++){
-                if(a[i] % 2 == 0)
-                    count_even++;
-                else
-                    count_odd++;
-            }
-            if(count_even == count_odd)
-                cout<<"0"<<endl;
-            else if(count_even > count_odd)
-                
+
+        long long operations_count = 0; 
+
+        for (long long i = 0; i < n - 1; i++) 
+        {
+            
+            if ((a[i] % 2) == (a[i + 1] % 2))
+                operations_count++; 
+        }
+        cout << operations_count << endl; 
     }
+    return 0;
 }
