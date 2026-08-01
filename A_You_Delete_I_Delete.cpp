654b480384c65e137ce4ff1daffab77c97b2e44c
akshvy <akshayroy217@gmail.com>
2026-08-01 23:56:35 +0530

    Add implementation for A. You Delete, I Delete problem from Codeforces Round 1113

---
+++ A_You_Delete_I_Delete.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s; 
+        cin>>s;
+
+    int pos0 = s.find('0');
+        s.erase(pos0, 1);
+
+    int pos1 = s.find('1');
+        s.erase(pos1, 1);
+
+        cout << s << endl;
+
+        
+    }
+    return 0;
+}
